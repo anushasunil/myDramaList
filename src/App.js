a8d546d013495959a38ce9621c11fe168eae33dc
Anusha <90134948+anushasunil@users.noreply.github.com>
2021-09-22 17:16:12 +0530

    refactor: updated description

--- src/App.js
+++ src/App.js
@@ -7,31 +7,61 @@ export default function App() {
     thriller: [
       {
         name: "Taxi Driver (2021)",
-        ratings: "9.8/10"
+        description:
+          "Based on real crimes cases, Rainbow Taxi Company provides revenge-seeking services to the people who lost their loved ones as victims to horrendous crimes. ",
+        ratings: "⭐9.8/10"
       },
       {
-        name: "Mr Sunshine (2018)",
-        ratings: "9/10"
+        name: "Sky Castle (2018)",
+        description:
+          "The story revolves around 4 women living in luxurious residential complex named Sky Castle, potraying the jealousy,egoism and betrayal among themselves under the mask, as they compete to get their children accepted in Seoul National University by 'any' means.",
+        ratings: "⭐9/10"
+      },
+      {
+        name: "Kingdom (2019)",
+        description:
+          "The prince is kept in dark about his highness' whereabout, his investigation led him to a mysterious plague which resurrects the dead.He fights his way to unveil the truth behind this brutality.",
+        ratings: "⭐9/10"
       }
     ],
     romance: [
       {
+        name: "When My Love Blooms (2020)",
+        description:
+          "Yoon Ji Soo and Han Jae Hyeon accidently reunite after 26 years, as they take us to a rollercoaster ride of emotions, the drama beautifully blends their memories of being each others interests in their college days with the present.",
+        ratings: "⭐10/10"
+      },
+      {
         name: "Moon Lovers: Scarlet Heart Ryeo (2016)",
-        ratings: "9.5/10"
+        description:
+          "Go Ha jin, who was at the rock bottom of her life, travels 1000 years back in Goryeo dynasty, gets entangled in the brutality of royal kingdom.",
+        ratings: "⭐9.5/10"
       },
       {
         name: "True Beauty (2020)",
-        ratings: "8/10"
+        description:
+          "Based on the webtoon with same title, Im Joo Kyung is a high-schooler, bullied because of her looks hides this past under her makeup skills as no one could figure out her secret from the after-makeup results",
+        ratings: "⭐8/10"
       }
     ],
     comedy: [
       {
         name: "Eulachacha Waikiki (2018)",
-        ratings: "8.5/10"
+        description:
+          "If you are super bored in life, give it a watch. You'll die laughing. The drama revolves around 'daily' life incident of 3 friends living together, running a not-so-success guesthouse.",
+        ratings: "⭐8.5/10"
       },
       {
         name: "Strong woman Do Bong Soon (2017)",
-        ratings: "8/10"
+        description:
+          "Better known for breaking stereotypes of all romance-themed kdramas. Do Bong Soon was born with superpowers and she's hired by a CEO, to be his bodyguard",
+        ratings: "⭐8/10"
+      },
+      {
+        name: "Weightlifting fairy Kim Bok Joo(2017)",
+        description:
+          "Drama revolves around rookie weightlifter Kim Bok Joo and her friends at National Sports Academy",
+        ratings: "⭐8/10"
       }
     ]
   };
@@ -56,6 +86,8 @@ export default function App() {
           return (
             <li>
               <span style={{ fontWeight: "bold" }}>{itemName.name}</span>
+              <br />
+              <span style={{ fontSize: "1rem" }}>{itemName.description}</span>
               <br />
               <span style={{ fontSize: "1rem" }}>{itemName.ratings}</span>
             </li>
